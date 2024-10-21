cb87155fbdd56e8705aca89c297a31b027ed787b
Nik Hixson <nikilobe@gmail.com>
2024-10-21 16:17:09 -0700

    updated with the toggle answer code so I could have a good reference for the future

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,16 +1,15 @@
 const fs = require('fs');
 
-const breedDetailsFromFile = (breed, callback) => {
+const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
   console.log('breedDetailsFromFile: Calling readFile...');
-  fs.readFile(`./data/${breed}.txt`, 'utf8', (err, data) => {
-    if (err) {
-      return callback(null);
-    }
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
-    callback(data);
+    if (!error) functionToRunWhenThingsAreDone(data);
   });
 };
 
-breedDetailsFromFile('Bombay', (data) => {
-  console.log('Return Value:', data);
-});
+const printOutCatBreed = breed => {
+  console.log('Return Value: ', breed);
+};
+
+breedDetailsFromFile('Bombay', printOutCatBreed);
